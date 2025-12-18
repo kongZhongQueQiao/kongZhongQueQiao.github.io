98e3b57daf07b6d85800a619e22f96177e25ca09
kongZhongQueQiao <1956916009@qq.com>
2025-12-18 09:05:32 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/gesture-christmas-tree-2/',
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
+    //   base: '/gesture-christmas-tree-2/' 仓库名,
+    base: '/kongZhongQueQiao.github.io/',
+    server: {
+        port: 3000,
+        host: '0.0.0.0',
+    },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, '.'),
+        }
     }
-  }
 });
